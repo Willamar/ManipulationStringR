fa01c328c76a2fb685f3b559c83e3f17f779e269
Willamar <wfs20@hotmail.com>
2019-01-04 21:19:52 -0200

    Atualizaçãoo com novos comandos basicos de manipulação de string com R

--- String Basics.R
+++ String Basics.R
@@ -66,3 +66,105 @@ format(income, digits=2)
 format(p_values)
 # Format p_values in fixed format
 format(p_values, scientific=FALSE)
+
+
+formatted_income <- format(income, digits = 2)
+
+# Print formatted_income
+formatted_income
+
+# Call writeLines() on the formatted income
+writeLines(formatted_income)
+
+# Define trimmed_income
+trimmed_income <- format(income, digits = 2, trim = TRUE)
+
+# Call writeLines() on the trimmed_income
+writeLines(trimmed_income)
+
+# Define pretty_income
+pretty_income <- format(income, digits = 2, big.mark = ",")
+
+# Call writeLines() on the pretty_income
+writeLines(pretty_income)
+
+
+# From the format() exercise
+x <- c(0.0011, 0.011, 1)
+y <- c(1.0011, 2.011, 1)
+
+# formatC() on x with format = "f", digits = 1
+formatC(x, format="f", digits = 1)
+
+# formatC() on y with format = "f", digits = 1
+formatC(y, format = "f", digits = 1)
+
+# Format percent_change to one place after the decimal point
+formatC(percent_change, format = "f", digits = 1)
+
+# percent_change with flag = "+"
+pretty_percent <- formatC(percent_change, format = "f", digits = 1, flag = "+")
+pretty_percent
+
+# Format p_values using format = "g" and digits = 2
+formatC(p_values, format = "g", digits=2)
+
+years <- 2010:2013
+
+
+
+# Add $ to pretty_income
+paste("$", pretty_income, sep="")
+
+# Add % to pretty_percent
+paste(pretty_percent,"%", sep="")
+
+# Create vector with elements like 2010: +4.0%`
+year_percent <- paste(years, paste(pretty_percent, "%", sep = ""), sep=": ")
+
+# Collapse all years into single string
+paste(year_percent, collapse=", ")
+
+# Define the names vector
+income_names <- c("Year 0", "Year 1", "Year 2", "Project Lifetime")
+
+# Create pretty_income
+pretty_income <- format(income, digits = 2, big.mark = ",")
+
+# Create dollar_income
+dollar_income <- paste("$", pretty_income, sep="")
+
+# Create formatted_names
+formatted_names <- format(income_names, justify="right")
+
+# Create rows
+rows <- paste(formatted_names, dollar_income, sep="   ")
+
+# Write rows
+writeLines(rows)
+
+toppings <- c("anchovies", "artichoke", "bacon"    , "breakfast bacon", "Canadian bacon"  , "cheese"   , "chicken"  , "chili peppers"   
+      , "feta"     , "garlic"   , "green peppers"   , "grilled onions"  
+      , "ground beef",      "ham"      , "hot sauce", "meatballs"       
+      , "mushrooms", "olives"   , "onions"   , "pepperoni"       
+      , "pineapple", "sausage"  , "spinach"  , "sun-dried tomato"
+      , "tomatoes")
+
+# Randomly sample 3 toppings
+my_toppings <- sample(toppings, size = 3)
+
+# Print my_toppings
+my_toppings
+
+# Paste "and " to last element: my_toppings_and
+my_toppings_and <- paste(c("","","and "), my_toppings, sep="")
+
+# Collapse with comma space: these_toppings
+these_toppings <- paste(my_toppings_and, collapse=", ")
+
+# Add rest of sentence: my_order
+my_order <- paste("I want to order a pizza with ", these_toppings, ".", sep="")
+
+# Order pizza with writeLines()
+writeLines(my_order)
+
